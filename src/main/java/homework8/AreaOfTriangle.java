a0aa151dcc6449623d6f05b85add47a36ae9704d
rosist <rositaneva@gmail.com>
2023-01-29 18:14:20 +0200

    Final version

--- src/main/java/homework8/AreaOfTriangle.java
+++ src/main/java/homework8/AreaOfTriangle.java
@@ -20,12 +20,10 @@ public class AreaOfTriangle {
             System.out.println("Invalid values for triangle sides");
         }
     }
-
     private double calculateArea(double a, double b, double c) {
         double p = (a + b + c) / 2;
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
-
     private boolean isTriangleValid(double a, double b, double c) {
         if (a > 0 && b > 0 && c > 0 && (a + b) > c && (a + c) > b && (b + c) > a) {
             return true;
