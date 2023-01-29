f6d34b3c33a86906a98fb67b77f25b573f7cffdf
rosist <rositaneva@gmail.com>
2023-01-29 15:14:24 +0200

    v1

--- src/main/java/javaIntro/Intro.java
+++ src/main/java/javaIntro/Intro.java
@@ -2,22 +2,27 @@ package javaIntro;
 
 import java.util.Scanner;
 
-    public class Intro {
-        public static void main(String[]args){
-            System.out.println("Please enter your name:");
-            Scanner scanner = new Scanner(System.in);
-            String name = scanner.nextLine();
+public class Intro {
+    public static void main(String[] args) {
+        System.out.println("Please enter your name:");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
 
-            System.out.println("Please, enter your age");
-            int age = scanner.nextInt();
+        System.out.println("Please, enter your age");
+        int age = scanner.nextInt();
 
-            System.out.println("Hi, "+ name+ " your age is " +age);
+        System.out.println("Hi, " + name + " your age is " + age);
 
-            if (age>=18) {
-                System.out.println(name + " is able to drink.");}
-                else {System.out.println(name + " is not able to drink.");}
-            }
+        if (age >= 18) {
+            System.out.println(name + " is able to drink.");
+        } else {
+            System.out.println(name + " is not able to drink.");
         }
+
+
+    }
+}
+
 
 
 
